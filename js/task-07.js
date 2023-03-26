4ef6f8e9f53acb2b83a49f19a0e6730fe9624615
Inna-Melnyk <inamelnyk@ukr.net>
2023-03-26 23:16:59 +0200

    task 7

--- js/task-07.js
+++ js/task-07.js
@@ -1,27 +1,3 @@
-// const inputEl = document.querySelector("#font-size-control");
-// console.log(inputEl);
-
-// const textEl = document.querySelector('#text')
-
-// inputEl.addEventListener('input', onInput);
-
-// function onInput(evt) {
-//     console.log(evt);
-//     // textEl.style.fontSize = "96px";
-//     // const valueOfRangeElement = 16;
-
-//     // const NewFontSize = valueOfRangeElement.toString() + "px";
-//     const size = 16;
-//     textEl.style.fontSize = size + 'px';
-// }
-
-// var slider = document.getElementById("font-size");
-
-// slider.addEventListener("input", function () {
-//   var size = slider.value;
-//   // this sets the body's font size property, but you can set whatever you need to
-//   document.body.style.fontSize = size + "px";
-// });
 
 const rangeElement = document.getElementById("font-size-control");
 
