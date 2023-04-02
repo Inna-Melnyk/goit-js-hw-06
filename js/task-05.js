b05588d67876324266c8a108b3913405e5d8042e
Inna-Melnyk <inamelnyk@ukr.net>
2023-04-02 12:17:09 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -5,7 +5,7 @@ const titleEl = document.querySelector("#name-output");
 inputEl.addEventListener("input", onInput);
 
 function onInput(evt) {
-  if (evt.currentTarget.value == false) {
+  if (!evt.currentTarget.value ) {
     titleEl.textContent = "Anonymous";
   } else {
     titleEl.textContent = evt.currentTarget.value.trim();
